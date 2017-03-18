7e89cfc73a24b600343a3e60715f1df775b97ac8
Mihail Kopchev <mihailkopchev@Mihails-MacBook-Air.local>
2017-03-18 19:22:06 +0200

    delete call work

--- src/org/webserver/DeleteHttpCall.java
+++ src/org/webserver/DeleteHttpCall.java
@@ -1,25 +1,71 @@
 package org.webserver;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DeleteHttpCall implements HttpCall {
 
+	private static Validator validator;
+	private List<String> parameters = new ArrayList<String>();
+	static {
+		validator = new Validator() {
+
+			public boolean validateFile(String f) {
+				File file = new File(f);
+				boolean exist = file.exists();
+				return exist;
+			}
+
+		};
+	}
+
 	@Override
 	public void execute(HttpRequest req, HttpResponse resp) {
-
-		String uri = req.getUri();
-		String filePath = FILESYSTEM + uri;
-		File file = new File(filePath);
-
-		if (file.exists()) {
-			file.delete();
-			resp.sendOK();
-		} else {
-			resp.sendError(HTTPCode.SERVERERROR);
+		String filePath = FILESYSTEM + req.getUri();
+		if (!validator.validateFile(filePath)) {
+			resp.sendError(HTTPCode.ERROR);
+			return;
 		}
+		File currentFile = new File(filePath);
+		if (currentFile.isFile()) {
+			this.deleteFile(filePath);
+		} else {
+			this.deleteDirectory(filePath);
+		}
+		resp.sendOK();
+	}
+
+	private void deleteFile(String filePath) {
+		File file = new File(filePath);
+		if (file != null) {
+			file.delete();
+		}
+	}
+
+	private void deleteDirectory(String filePath) {
+		File file = new File(filePath);
+		if (file != null) {
+			String[] files = file.list();
+			for (String currentFile : files) {
+				if (currentFile.endsWith(".DS_Store")) {
+					File f = new File(filePath + File.separator + currentFile);
+					f.delete();
+					continue;
+				}
+			File f = new File(filePath + File.separator + currentFile);
+			if (f.isFile()) {
+				f.delete();
+			} else {
+				deleteDirectory(filePath + File.separator + currentFile);
+			}
+		}
+		file.delete();
+	}
+
+	}
+
+	private void delete() {
 
 	}
 
